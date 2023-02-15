e162163485a3334f17db623088f6e4c847022933
Evellyin <evellyin.developer@gmail.com>
2023-02-15 09:10:57 -0300

    Adicionar POST de cadastro de pacientes.

---
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,6 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereço.DadosEndereco;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
+}
