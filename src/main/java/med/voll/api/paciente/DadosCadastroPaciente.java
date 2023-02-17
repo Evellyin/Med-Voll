be2224eb2974ac46d308e9b9f6fe21883c765a46
Evellyin <evellyin.developer@gmail.com>
2023-02-17 11:37:09 -0300

    Adicionar bean validations em dtos de paciente e medico.

--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,6 +1,23 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
+import org.hibernate.validator.constraints.br.CPF;
 
-public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{8,11}")
+        String telefone,
+        @NotBlank
+        @CPF
+        String cpf,
+        @NotBlank
+        DadosEndereco endereco) {
 }
