24312073d6f189bf42aa9f633670e0d110c34e2c
Evellyin <evellyin.developer@gmail.com>
2023-02-21 12:11:48 -0300

    Criar classe para tratamento de erros.

--- src/main/java/med/voll/api/domain/endereco/Endereco.java
+++ src/main/java/med/voll/api/domain/endereco/Endereco.java
@@ -1,11 +1,10 @@
-package med.voll.api.endereco;
+package med.voll.api.domain.endereco;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.paciente.DadosCadastroPaciente;
 
 @Embeddable
 @Getter
--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -1,25 +1,26 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
-import org.hibernate.validator.constraints.br.CPF;
+import med.voll.api.domain.endereco.DadosEndereco;
 
-public record DadosCadastroPaciente(
+public record DadosCadastroMedico(
         @NotBlank
         String nome,
+        @NotBlank
+        String telefone,
         @NotBlank
         @Email
         String email,
         @NotBlank
-        @Pattern(regexp = "\\d{8,11}")
-        String telefone,
-        @NotBlank
-        @CPF
-        String cpf,
-        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
         @Valid
         DadosEndereco endereco) {
 }
