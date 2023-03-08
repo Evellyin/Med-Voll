f7b070e6320d7e2906ee65a129b7376ac172221d
Evellyin <evellyin.developer@gmail.com>
2023-03-08 16:23:18 -0300

    Criar método de agendamento de consultas.

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -22,6 +22,8 @@ public class ConsultaController {
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        agendaDeConsultas.agendar(dados);
+
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 
